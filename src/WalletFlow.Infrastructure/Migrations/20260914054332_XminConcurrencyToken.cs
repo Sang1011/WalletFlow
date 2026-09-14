1a147e2228557495ad528f84c3325ee5bfc93112
Sang <Sang101174@gmail.com>
2026-09-14 12:44:03 +0700

    chore(db): add XminConcurrencyToken migration for Wallet.Version

---
+++ src/WalletFlow.Infrastructure/Migrations/20260914054332_XminConcurrencyToken.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WalletFlow.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class XminConcurrencyToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
